0688bf58876dc13de054fede85af8ff4d19afbc1
benny <benny08y@gmail.com>
2019-03-04 21:37:45 -0800

    wave implemented

--- gameengine.js
+++ gameengine.js
@@ -37,6 +37,7 @@ function GameEngine() {
     this.wheel = null;
     this.surfaceWidth = null;
     this.surfaceHeight = null;
+    this.pondArray;
 }
 
 GameEngine.prototype.init = function (ctx) {
